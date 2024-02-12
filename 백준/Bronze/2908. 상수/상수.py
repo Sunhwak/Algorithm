3f18ff67782bef0323c403cdda73917de89d004d
Sunhwak <157467969+Sunhwak@users.noreply.github.com>
2024-02-12 20:11:14 +0900

    [Bronze II] Title: 상수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,9 +1,5 @@
-listA = list(map(str, input().split()))
-reversed_listA = []
+a, b = map(str, input().split())
+a = int(a[::-1])
+b = int(b[::-1])
 
-for ele in listA :
-    ele = ele[::-1]
-    reversed_listA.append(int(ele))
-
-
-print(max(reversed_listA))
+print(max(a, b))
