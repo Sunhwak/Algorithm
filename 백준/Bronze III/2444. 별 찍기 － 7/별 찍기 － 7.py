7050152d4d18611e6de67fd170493bc49656c277
Sunhwak <157467969+Sunhwak@users.noreply.github.com>
2024-02-15 20:34:01 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze III/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze III/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,15 +1,7 @@
 n = int(input())
 
-for i in range(n) :
-    for j in range(n-i-1) :
-        print(' ', end = '')
-    for k in range((i+1)*2-1) :
-        print('*', end = '')
-    print()
+for i in range(1, n) :
+    print(' '*(n-i)+'*'*(i*2-1))
 
-for i in range(n-2, -1, -1) :
-    for j in range(n-i-1) :
-        print(' ', end='')
-    for k in range((i + 1) * 2 - 1):
-        print('*', end='')
-    print()
+for i in range(n, -1, -1) :
+    print(' '*(n-i)+'*'*(i*2-1))
